49f6e15be0862078e10aaf329151fa3d849c6ebd
mostofa <mdmostofakhan1972@gmail.com>
2023-03-07 12:18:11 +0600

    other

--- js/phone.js
+++ js/phone.js
@@ -34,12 +34,14 @@ const displayPhones = (phones, dataLimit) => {
     const phoneDiv = document.createElement('div');
     phoneDiv.classList.add('col');
     phoneDiv.innerHTML = `
-         <div class="card p-4">
+       <div class="card p-4">
           <img src="${phone.image}" class="card-img-top" alt="...">
           <div class="card-body">
-          <h5 class="card-title">${phone.phone_name}</h5>
-         <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-        </div>
+            <h5 class="card-title">${phone.phone_name}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <button onclick="loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#phoneDetailModal">Go somewhere</button>
+           </div>
+            
       </div>
     `;
         phoneContainer.appendChild(phoneDiv);
@@ -61,6 +63,14 @@ document.getElementById('btn-search').addEventListener('click',function(){
       processSearch(10)
 })
 
+// search input field enter key handler
+document.getElementById('search-phone').addEventListener('keypress', function (e) {
+  if (e.key === 'Enter') {
+    processSearch(10)
+  }
+});
+
+
 const toggolSpinner = losding => {
   const loder = document.getElementById('loder')
   if(losding){
@@ -75,4 +85,25 @@ const toggolSpinner = losding => {
 document.getElementById('btn-show-all').addEventListener('click', function(){
     processSearch();
 })
-// loadPhone();
+
+const loadPhoneDetails = async id =>{
+  const url = `https://openapi.programming-hero.com/api/phone/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displaPhoneDetails(data.data);
+}
+
+const displaPhoneDetails = phone => {
+  console.log(phone)
+  const modalTitel = document.getElementById('phoneDetailModalLabel')
+ modalTitel.innerText = phone.name;
+ const modalBody = document.getElementById('modal-body')
+ modalBody.innerHTML = `
+ <img src="${phone.image}" alt="">
+ <p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'no Release Date Founction' }</p>
+ <p>Other: ${phone.others ? phone.others.Bluetooth : 'no Bluetooth informatiion'}</p>
+ <p>Main Features: ${phone.mainFeatures ? phone.mainFeatures.chipSet : 'no chipSet information'}
+ `;
+}
+
+loadPhone('apple');
